2b5b5f591c10aff58154269a8bc11b1982f20885
yuenfu001 <liuyuenfu@gmail.com>
2025-04-22 20:03:54 +0100

    updated the tests.py

--- todo_list/tests.py
+++ todo_list/tests.py
@@ -1,9 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
+from .models import Todo
+class HomrViewTest(TestCase):
 
-class SimpleViewTests(TestCase):
+    def setup(self):
+        #create a todo
+        Todo.objects.create(title="Todo", description='Test 1')
+        Todo.objects.create(title="Todo 1", description='Test 2')
+        self.client = Client()
 
     def test_home_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code,200)
-        self.assertContains(response, "This is my home page")
+    
+    def test_home_view_uses_correct_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response,'index.html')
+
+    def test_home_view_context_data(self):
+        
+        response = self.client.get(reverse('home'))
+        self.assertIn('list',response.context)
+        self.assertEqual(len(response.context['list']),2)
