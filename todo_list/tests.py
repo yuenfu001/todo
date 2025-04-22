0e37cf93de307633ee982326f0dad20d9283dbd3
yuenfu001 <liuyuenfu@gmail.com>
2025-04-22 11:39:28 +0100

    added test

--- todo_list/tests.py
+++ todo_list/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SimpleViewTests(TestCase):
+
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, "This is my home page")
