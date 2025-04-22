d17b778acd5aa58b4056dba5b2d74b52c85890ff
yuenfu001 <liuyuenfu@gmail.com>
2025-04-22 10:30:44 +0100

    added return to my views

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpRequest
 # Create your views here.
+
 def home(request):
-    HttpResponse("This is my home page")
+    return HttpResponse("This is my home page")
